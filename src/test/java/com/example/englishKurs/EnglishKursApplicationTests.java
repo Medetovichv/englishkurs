69a0640f6c72cf800ac726e23508d14935dab510
medetovich <said.medetovich@gmail.com>
2023-02-26 12:38:29 +0100

    first commit

---
+++ src/test/java/com/example/englishKurs/EnglishKursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.englishKurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishKursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
